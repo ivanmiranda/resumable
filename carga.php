4dad1d20421695112e8ee455945f6080c431fbca
ivan miranda <ivan@pengostores.com>
2016-02-10 11:05:56 -0600

    Demo

--- carga.php
+++ carga.php
@@ -1,53 +1,13 @@
 <?php
-/**
- * This is the implementation of the server side part of
- * Resumable.js client script, which sends/uploads files
- * to a server in several chunks.
- *
- * The script receives the files in a standard way as if
- * the files were uploaded using standard HTML form (multipart).
- *
- * This PHP script stores all the chunks of a file in a temporary
- * directory (`temp`) with the extension `_part<#ChunkN>`. Once all 
- * the parts have been uploaded, a final destination file is
- * being created from all the stored parts (appending one by one).
- *
- * @author Gregory Chris (http://online-php.com)
- * @email www.online.php@gmail.com
- *
- * @editor Bivek Joshi (http://www.bivekjoshi.com.np)
- * @email meetbivek@gmail.com
- */
-
-
-////////////////////////////////////////////////////////////////////
-// THE FUNCTIONS
-////////////////////////////////////////////////////////////////////
-
-/**
- *
- * Logging operation - to a file (upload_log.txt) and to the stdout
- * @param string $str - the logging string
- */
 function _log($str) {
-
-    // log to the output
     $log_str = date('d.m.Y').": {$str}\r\n";
     echo $log_str;
-
-    // log to file
-    if (($fp = fopen('upload_log.txt', 'a+')) !== false) {
+    if (($fp = fopen('log.txt', 'a+')) !== false) {
         fputs($fp, $log_str);
         fclose($fp);
     }
 }
 
-/**
- * 
- * Delete a directory RECURSIVELY
- * @param string $dir - directory path
- * @link http://php.net/manual/en/function.rmdir.php
- */
 function rrmdir($dir) {
     if (is_dir($dir)) {
         $objects = scandir($dir);
@@ -65,18 +25,8 @@ function rrmdir($dir) {
     }
 }
 
-/**
- *
- * Check if all the parts exist, and 
- * gather all the parts of the file together
- * @param string $temp_dir - the temporary directory holding all the parts of the file
- * @param string $fileName - the original file name
- * @param string $chunkSize - each chunk size (in bytes)
- * @param string $totalSize - original file size (in bytes)
- */
 function createFileFromChunks($temp_dir, $fileName, $chunkSize, $totalSize,$total_files) {
-
-    // count all the parts of this file
+    // cuenta las partes del archivo
     $total_files_on_server_size = 0;
     $temp_total = 0;
     foreach(scandir($temp_dir) as $file) {
@@ -84,23 +34,22 @@ function createFileFromChunks($temp_dir, $fileName, $chunkSize, $totalSize,$tota
         $tempfilesize = filesize($temp_dir.'/'.$file);
         $total_files_on_server_size = $temp_total + $tempfilesize;
     }
-    // check that all the parts are present
-    // If the Size of all the chunks on the server is equal to the size of the file uploaded.
+    // revisa si cada parte está en el directorio
+    //Si el tamaño de las partes es igual al del archivo subido
     if ($total_files_on_server_size >= $totalSize) {
-    // create the final destination file 
+    // Directorio final
         if (($fp = fopen('temp/'.$fileName, 'w')) !== false) {
             for ($i=1; $i<=$total_files; $i++) {
                 fwrite($fp, file_get_contents($temp_dir.'/'.$fileName.'.part'.$i));
-                _log('writing chunk '.$i);
+                _log('escribiendo parte '.$i);
             }
             fclose($fp);
         } else {
-            _log('cannot create the destination file');
+            _log('no se pudo escribir el archivo final');
             return false;
         }
 
-        // rename the temporary directory (to avoid access from other 
-        // concurrent chunks uploads) and than delete it
+        // renombra el directorio (para evitar sobreescritura de piezas)
         if (rename($temp_dir, $temp_dir.'_UNUSED')) {
             rrmdir($temp_dir.'_UNUSED');
         } else {
@@ -110,14 +59,7 @@ function createFileFromChunks($temp_dir, $fileName, $chunkSize, $totalSize,$tota
 
 }
 
-
-////////////////////////////////////////////////////////////////////
-// THE SCRIPT
-////////////////////////////////////////////////////////////////////
-
-//check if request is GET and the requested chunk exists or not. this makes testChunks work
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-
     if(!(isset($_GET['resumableIdentifier']) && trim($_GET['resumableIdentifier'])!='')){
         $_GET['resumableIdentifier']='';
     }
@@ -136,32 +78,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
        }
 }
 
-// loop through files and move the chunks to a temporarily created directory
 if (!empty($_FILES)) foreach ($_FILES as $file) {
-
-    // check the error status
     if ($file['error'] != 0) {
         _log('error '.$file['error'].' in file '.$_POST['resumableFilename']);
         continue;
     }
 
-    // init the destination file (format <filename.ext>.part<#chunk>
-    // the file is stored in a temporary directory
+    // archivo destino (format <filename.ext>.part<#chunk>
     if(isset($_POST['resumableIdentifier']) && trim($_POST['resumableIdentifier'])!=''){
         $temp_dir = 'temp/'.$_POST['resumableIdentifier'];
     }
     $dest_file = $temp_dir.'/'.$_POST['resumableFilename'].'.part'.$_POST['resumableChunkNumber'];
 
-    // create the temporary directory
     if (!is_dir($temp_dir)) {
         mkdir($temp_dir, 0777, true);
     }
 
-    // move the temporary file
     if (!move_uploaded_file($file['tmp_name'], $dest_file)) {
         _log('Error saving (move_uploaded_file) chunk '.$_POST['resumableChunkNumber'].' for file '.$_POST['resumableFilename']);
     } else {
-        // check if all the parts present, and create the final destination file
+        // checa las partes cargadas y crea el archivo
         createFileFromChunks($temp_dir, $_POST['resumableFilename'],$_POST['resumableChunkSize'], $_POST['resumableTotalSize'],$_POST['resumableTotalChunks']);
     }
 }
---
+++ cargas.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sfphp</title>
+  <!-- Bootstrap Table -->
+  <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+  <script src="resumable.js"></script>
+  <script src="loader.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php
+foreach(glob($log_directory.'./temp/*.*') as $file) {
+    echo "<a href='".$file."'>".str_replace("./temp/", "", $file)." (".number_format(((filesize($file)/1024)/1024),2)."MB)</a><br>";
+}
