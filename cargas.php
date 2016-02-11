fa10c30ea1a2b795c995ca4f94d86ad3c45dd678
ivan miranda <ivan@pengostores.com>
2016-02-10 19:05:57 -0600

    Demo

--- cargas.php
+++ cargas.php
@@ -10,9 +10,27 @@
   <script src="loader.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/bootstrap.filestyle/1.1.0/js/bootstrap-filestyle.min.js"></script>
 </head>
 <body>
+
+<div class="page-header">
+  <h1>Demo de carga asincrona <small> y previo de imagen sin carga de imagen</small></h1>
+</div>
+
 <?php
 foreach(glob('./temp/*.*') as $file) {
-    echo "<a href='".$file."'>".str_replace("./temp/", "", $file)." (".number_format(((filesize($file)/1024)/1024),2)."MB)</a><br>";
-}
+?>
+<div class="media">
+  <div class="media-left media-middle">
+    <a href="<?php echo $file ?>">
+      <img class="media-object" width="150" height="150" src="<?php echo $file ?>" alt="<?php echo str_replace("./temp/", "", $file) ?>">
+    </a>
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading"><?php echo str_replace("./temp/", "", $file)." (".number_format(((filesize($file)/1024)/1024),2)."MB)" ?></h4>
+  </div>
+</div>
+<?php
+} ?>
+
